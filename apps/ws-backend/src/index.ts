d97d5e44205785679a8106e569aff37e681e8b50
satyam <satyam.kumar@acem.edu.in>
2025-03-18 19:51:35 +0530

    new

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,26 +1,44 @@
-import { WebSocketServer } from 'ws';
-import jwt, { JwtPayload }  from 'jsonwebtoken';
+import { JWT_SECRET } from '@repo/backend-common/config';
+import { WebSocketServer } from "ws";
+
+import jwt, { JwtPayload } from "jsonwebtoken";
 const wss = new WebSocketServer({ port: 8080 });
 
-wss.on('connection', function connection(ws,request) {
-  
+function checkUser(token:string): string | null {
+
+const decoded= jwt.verify(token,JWT_SECRET);
+if(typeof decoded == "string"){
+  return null
+}
+if(!decoded || !decoded.userId){
+  return null;
+}
+return decoded.userId
+
+}
+
+wss.on("connection", function connection(ws, request) {
   const url = request.url;
   if (!url) {
     return;
   }
-  const queryParams = new URLSearchParams(url.split('?')[1]);
-  const token = queryParams.get('token') || "";
-  const decoded = jwt.verify(token,"12345");
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+  const token = queryParams.get("token") || "";
+ const userId= checkUser(token);
 
-
-  if (!decoded || !(decoded as JwtPayload ).userId) {
-    ws.close()
+  if (!userId) {
+    ws.close();
     return null;
   }
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  
+
+
+
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
   });
 
-  ws.send('something');
+  ws.send("something");
 });
